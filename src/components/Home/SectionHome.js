2a06bad9073f132f40fc9916d4dd697bbc0f50d4
Anton <anton@oas.nu>
2020-03-23 22:34:55 +0100

    DEV: Only using portrait mode and worked on reveal

--- src/components/Home/SectionHome.js
+++ src/components/Home/SectionHome.js
@@ -56,15 +56,13 @@ const Description = styled('div')`
     line-height: 1.3;
     letter-spacing: 1.5px;
     opacity: 0;
-    transform: translateY(15px);
-    transition: all all ${theme.easings.reveal};
+    transition: ${theme.easings.reveal};
   }
 
   &.reveal {
     .title,
     .subTitle {
       opacity: 1;
-      transform: translateY(0px);
     }
   }
 `
@@ -84,38 +82,47 @@ const Item = styled('li')`
   overflow: hidden;
   transform-origin: 50% 50%;
   transform-style: preserve-3d;
-  max-width: 0%;
-  transition: ${theme.easings.primary},
-              maxWidth ${theme.easings.reveal};
+  transition: ${theme.easings.primary};
   z-index: 1;
 
-  ${Array(6).map((item, i) => css`
-    &:nth-child(${i+2}) {
+  ${[...Array(6)].map((item, i) => css`
+    &:nth-child(${i+1})::before {
       transition: ${theme.easings.primary},
-                  maxWidth ${theme.easings.reveal} calc(${i+1} * ${theme.easings.revealDelay});
+                  max-width ${theme.easings.reveal} ${i * parseInt(theme.easings.revealDelay)}ms;
     }
   `)}
 
-  &.reveal {
-    max-width: 110%;
-  }
-
-  &::before {
+  &::before,
+  &::after {
     display: block;
     position: absolute;
     content: "";
     bottom: 0px;
     width: 100%;
-    height: calc( 100% + 25px);
+    height: 100%;
+  }
+
+  &::before {
+    left: 50%;
+    max-width: 0%;
+    background-color: ${({backgroundColor}) => backgroundColor};
+    transform: translateX(-50%);
+    z-index: 1;
+  }
+
+  &.reveal::before {
+    max-width: 100%;
+  }
+
+  &::after {
     transition: ${theme.easings.primary};
     opacity: 0;
     transform-style : preserve-3d;
-    transform: translateY(25px);
     background-image: url('${({backgroundImage}) => backgroundImage}');
     background-position: center center;
     background-repeat: no-repeat;
     background-size: cover;
-    z-index: 1;
+    z-index: 2;
 
     ${theme.above.md} {
       background-image: url('${({backgroundImageDesktop}) => backgroundImageDesktop}');
@@ -136,7 +143,7 @@ const Item = styled('li')`
     white-space: nowrap;
     transition: ${theme.easings.primary};
     transform-style : preserve-3d;
-    z-index: 2;
+    z-index: 3;
     outline: none;
   }
 
@@ -152,9 +159,8 @@ const Item = styled('li')`
   }
 
   &.active {
-    &::before {
+    &::after {
       opacity: 0.3;
-      transform: translateY(0px);
     }
 
     a {
@@ -173,10 +179,10 @@ class MainNavigation extends Component {
 
   componentDidMount() {
     window.addEventListener('resize', this.handleResize, false)
-    window.addEventListener('wheel', this.handleScroll, false)
     window.addEventListener('keydown', this.handleKey, false)
-    window.addEventListener('touchmove', this.handleMove, false);
-    window.addEventListener('touchend', this.handleMoveEnd, false);
+    //window.addEventListener('wheel', this.handleScroll, false)
+    //window.addEventListener('touchmove', this.handleMove, false);
+    //window.addEventListener('touchend', this.handleMoveEnd, false);
 
     this.setState({
       items: this.props.items
@@ -188,17 +194,16 @@ class MainNavigation extends Component {
     clearTimeout(this.scrollTimeout);
     clearTimeout(this.awaitTabTimeout);
     window.removeEventListener('resize', this.handleResize, false)
-    window.removeEventListener('wheel', this.handleScroll, false)
     window.removeEventListener('keydown', this.handleKey, false)
-    window.removeEventListener('touchmove', this.handleMove, false)
-    window.removeEventListener('touchend', this.handleMoveEnd, false)
+    //window.removeEventListener('wheel', this.handleScroll, false)
+    //window.removeEventListener('touchmove', this.handleMove, false)
+    //window.removeEventListener('touchend', this.handleMoveEnd, false)
   }
 
   state = {
     prevTouch: null,
     activeIndex: null,
     items: null, 
-    orientation: 'portrait',
     navWidth: 280,
     navHeight: 416
   }
@@ -207,10 +212,8 @@ class MainNavigation extends Component {
     if ( this.ref ) {
       let navWidth = this.ref.offsetWidth
       let navHeight = this.ref.offsetHeight
-      let newOrientation = navWidth > navHeight ? 'landscape' : 'portrait'
 
       this.setState({
-        orientation: newOrientation,
         navWidth: navWidth,
         navHeight: navHeight
       })
@@ -281,12 +284,12 @@ class MainNavigation extends Component {
     }
   }
 
-  handleScroll = e => {
+  /*handleScroll = e => {
     const scrollDirection = e.deltaY
     this.indexStep( scrollDirection )
-  }
+  }*/
 
-  handleMove = e => {
+  /*handleMove = e => {
     const { prevTouch } = this.state
     const { clientY } = e.touches[0]
     let scrollDirection = null
@@ -308,7 +311,7 @@ class MainNavigation extends Component {
     this.setState({
       prevTouch: null
     })
-  }
+  }*/
 
   setActiveIndex = e => {
     clearTimeout(this.scrollTimeout);
@@ -348,7 +351,7 @@ class MainNavigation extends Component {
 
   render () {
 
-    const { items, activeIndex, navWidth, navHeight, orientation } = this.state
+    const { items, activeIndex, navWidth, navHeight } = this.state
     const { reveal } = this.props
 
     return (
@@ -360,7 +363,6 @@ class MainNavigation extends Component {
 
           const isActive = activeIndex === i
           const notActive = activeIndex !== null && !isActive
-          const isPortrait = orientation === 'portrait'
 
           //Classes
           let itemClasses = isActive ? 'active' : ''
@@ -371,23 +373,19 @@ class MainNavigation extends Component {
           const defaultColor = lightenDarkenColor(theme.colors.black, (step * (items.length - i)))
           const baseColor = activeIndex !== null ? items[activeIndex].color : undefined
           const activeColor = activeIndex !== null && baseColor ? lightenDarkenColor(baseColor, (step * (activeIndex - i))) : undefined
-          const background = activeColor ? activeColor : defaultColor
+          const backgroundColor = activeColor ? activeColor : defaultColor
 
           //Default sizes
-          let width = isPortrait ? navWidth : (navWidth/items.length)
-          let height = isPortrait ? (navHeight/items.length) : navHeight
-          let top = isPortrait ? (i * height) : 0
-          let left = isPortrait ? 0 : (i * width)
+          let width = navWidth
+          let height = (navHeight/items.length)
+          let top = (i * height)
+          let left = 0
 
           //Sizes for portrait (mobile)
           let portraitWidthActive = 1.08
           let portraitheightActive = 1.4
-          width = isActive && isPortrait ? (width*portraitWidthActive) : width
-          height = isActive && isPortrait ? (height*portraitheightActive) : height
-
-          //Sizes for landscape (desktop)
-          height = isActive && !isPortrait ? height+50 : height
-          width = isActive && !isPortrait ? height : width
+          width = isActive ? (width*portraitWidthActive) : width
+          height = isActive ? (height*portraitheightActive) : height
 
           //Transform
           let difference = i - activeIndex
@@ -400,24 +398,14 @@ class MainNavigation extends Component {
           let translateY = 0
 
           if ( isActive ) {
-            if (isPortrait) {
-              translateX = `${(width - (width/portraitWidthActive))/-2}px`
-              translateY = `${(height - (height/portraitheightActive))/-2}px`
 
-            } else {
-              translateX = '0px'
-              translateY = '0px'
-
-            }
+            translateX = `${(width - (width/portraitWidthActive))/-2}px`
+            translateY = `${(height - (height/portraitheightActive))/-2}px`
 
           } else if ( notActive ) {
-            if ( isPortrait ) {
-              translateY = fromEnd === 0 ? 0 : `${position * (10/difference)}%`
 
-            } else {
-              translateX = fromEnd === 0 ? 0 : `${position * (10/difference)}%`
+            translateY = fromEnd === 0 ? 0 : `${position * (10/difference)}%`
 
-            }
           }
 
           return (
@@ -430,9 +418,9 @@ class MainNavigation extends Component {
                 height: height,
                 width: width,
                 zIndex: 10 - difference,
-                transform: `scaleX(${scale}) translate3d(${translateX}, ${translateY}, 0)`,
-                background: background
+                transform: `scaleX(${scale}) translate3d(${translateX}, ${translateY}, 0)`
               }}
+              backgroundColor={backgroundColor}
               backgroundImage={item.backgroundImage}
               backgroundImageDesktop={item.backgroundImageDesktop}
             >
@@ -440,7 +428,7 @@ class MainNavigation extends Component {
                 to={item.to}
                 className="mainNavLink"
                 onClick={e => {
-                  if ( isPortrait && !isActive ) {
+                  if ( isTouchDevice() && !isActive ) {
                     e.preventDefault();
                   }
                   this.setActiveIndex(e)
@@ -462,77 +450,112 @@ class MainNavigation extends Component {
   }
 }
 
-const SectionHome = ({ title, subTitle }) => {
+class SectionHome extends Component {
 
-  const items = [
-    {
-      title: "Mer om",
-      to: "/about",
-      color: "#99e9f2",
-      backgroundImage: "https://placekitten.com/660/270",
-      backgroundImageDesktop: "https://placekitten.com/800/800",
-    },
-    {
-      title: "Kunskap",
-      to: "/knowledge",
-      color: "#8ce99a",
-      backgroundImage: "https://placekitten.com/660/270",
-      backgroundImageDesktop: "https://placekitten.com/800/800",
-    },
-    {
-      title: "Erfarenhet",
-      to: "/experience",
-      color: "#ffd43b",
-      backgroundImage: "https://placekitten.com/660/270",
-      backgroundImageDesktop: "https://placekitten.com/800/800",
-    },
-    {
-      title: "Nybergs Bil",
-      topTitle: "Case",
-      to: "/nybergs-bil",
-      color: "#9e7b56",
-      backgroundImage: "https://placekitten.com/660/270",
-      backgroundImageDesktop: "https://placekitten.com/800/800",
-    },
-    {
-      title: "Svenska Hem",
-      topTitle: "Case",
-      to: "/svenska-hem",
-      color: "#e03131",
-      backgroundImage: "https://placekitten.com/660/270",
-      backgroundImageDesktop: "https://placekitten.com/800/800",
-    },
-    {
-      title: "OAS",
-      topTitle: "Case",
-      to: "/oas",
-      color: "#5b2160",
-      backgroundImage: "https://placekitten.com/660/270",
-      backgroundImageDesktop: "https://placekitten.com/800/800",
-    }
-  ]
+  componentDidMount() {
 
-  let descriptionReveal = true
-  let mainReveal = true
-  let footerReveal = true
+    const mainRevealDelay = 1000
+    const secondaryRevealDelay = 1000
 
-  return (
-    <HomeWrapper>
+    //Trigger main reveal
+    this.revealTimeOut = setTimeout(() => {
+      this.setState({
+        mainReveal: true
+      }, () => {
 
-      <Description className={descriptionReveal ? 'reveal' : ''}>
-        {title && <h1 className="title">{title}</h1>}
-        {subTitle && <h2 className="subTitle">{subTitle}</h2>}
-      </Description>
+        //Trigger secondary reveal
+        this.revealTimeOut = setTimeout(() => {
+          this.setState({
+            descriptionReveal: true,
+            footerReveal: true
+          })
+        }, secondaryRevealDelay)
+      })
+    }, mainRevealDelay)
+  }
 
-      <MainNavigation
-        items={items}
-        reveal={mainReveal}
-      />
+  componentWillUnmount() {
+    clearTimeout(this.revealTimeOut)
+    clearTimeout(this.revealTimeOut)
+  }
 
-      <Footer className={footerReveal ? 'reveal' : ''}/>
+  state = {
+    descriptionReveal: false,
+    mainReveal: false,
+    footerReveal: false
+  }
 
-    </HomeWrapper>
-  )
+  render () {
+
+    const { title, subTitle } = this.props
+    const { descriptionReveal, mainReveal, footerReveal } = this.state
+
+    const items = [
+      {
+        title: "Mer om",
+        to: "/about",
+        color: "#99e9f2",
+        backgroundImage: "https://placekitten.com/660/270",
+        backgroundImageDesktop: "https://placekitten.com/800/800",
+      },
+      {
+        title: "Kunskap",
+        to: "/knowledge",
+        color: "#8ce99a",
+        backgroundImage: "https://placekitten.com/660/270",
+        backgroundImageDesktop: "https://placekitten.com/800/800",
+      },
+      {
+        title: "Erfarenhet",
+        to: "/experience",
+        color: "#ffd43b",
+        backgroundImage: "https://placekitten.com/660/270",
+        backgroundImageDesktop: "https://placekitten.com/800/800",
+      },
+      {
+        title: "Nybergs Bil",
+        topTitle: "Case",
+        to: "/nybergs-bil",
+        color: "#9e7b56",
+        backgroundImage: "https://placekitten.com/660/270",
+        backgroundImageDesktop: "https://placekitten.com/800/800",
+      },
+      {
+        title: "Svenska Hem",
+        topTitle: "Case",
+        to: "/svenska-hem",
+        color: "#e03131",
+        backgroundImage: "https://placekitten.com/660/270",
+        backgroundImageDesktop: "https://placekitten.com/800/800",
+      },
+      {
+        title: "OAS",
+        topTitle: "Case",
+        to: "/oas",
+        color: "#5b2160",
+        backgroundImage: "https://placekitten.com/660/270",
+        backgroundImageDesktop: "https://placekitten.com/800/800",
+      }
+    ]
+
+    return (
+      <HomeWrapper>
+
+        <Description className={descriptionReveal ? 'reveal' : ''}>
+          {title && <h1 className="title">{title}</h1>}
+          {subTitle && <h2 className="subTitle">{subTitle}</h2>}
+        </Description>
+
+        <MainNavigation
+          items={items}
+          reveal={mainReveal}
+        />
+
+        <Footer className={footerReveal ? 'reveal' : ''}/>
+
+      </HomeWrapper>
+    )
+  }  
 }
 
 export default SectionHome
